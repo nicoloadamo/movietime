e8124ac30aeec9f1ccdd13dd89f3c6ce6eb6e224
Nicolo Adamo <nicolo.adamo@gmail.com>
2023-03-07 16:24:42 +0100

    initial validations done

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,7 @@ class Review < ApplicationRecord
   belongs_to :user
   belongs_to :event
   belongs_to :reviewer, class_name: "User"
+
+  validates :comment, :rating, presence: true
+  validates :comment, length: { minimum: 20, maximum: 500 }
 end
