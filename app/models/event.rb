26d5baf8ebf2398659dddce4393e85612fff3743
Aleks <48437522+a160v@users.noreply.github.com>
2023-03-08 10:54:22 +0100

    Update event.rb

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,7 +2,7 @@ class Event < ApplicationRecord
   belongs_to :movie
   belongs_to :user
   has_many :requests, dependent: :destroy
-  has_many :events, dependent: :nullify
+  # has_many :events, dependent: :nullify
 
   validates :name, :description, :language, :address, :start_time, :end_time, :max_registrations, presence: true
   validates :name, length: { minimum: 10, maximum: 60 }
