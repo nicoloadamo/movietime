d2488912b1f725c1a5d7af85be46e50c11a68eed
Nicolo Adamo <nicolo.adamo@gmail.com>
2023-03-09 02:28:38 +0100

    fixed time picker

--- app/javascript/controllers/flatpickr_controller.js
+++ app/javascript/controllers/flatpickr_controller.js
@@ -8,7 +8,19 @@ export default class extends Controller {
   static targets = [ "startTime", "endTime" ]
 
   connect() {
-    flatpickr(this.startTimeTarget, {})
-    flatpickr(this.endTimeTarget, {})
+
+    flatpickr(this.startTimeTarget, {
+      enableTime: true,
+      minDate: "today",
+      defaultDate: "19:00",
+      time_24hr: true,
+      dateFormat: "d-m-Y"
+    })
+    flatpickr(this.endTimeTarget, {
+      enableTime: true,
+      defaultDate: "22:00",
+      time_24hr: true,
+      dateFormat: "d-m-Y"
+    })
   }
 }
