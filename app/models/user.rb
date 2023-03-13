135b26e69e8bc8f6c3ebecf34176b388ead15750
nicoloadamo <122130648+nicoloadamo@users.noreply.github.com>
2023-03-14 00:09:51 +0100

    fixed structure and other useless code

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise  :database_authenticatable, :registerable,
+          :recoverable, :rememberable, :validatable
   has_many :requests # requests as a client
   has_many :requests_as_owner, through: :events, source: :events, dependent: :destroy
   has_many :events
