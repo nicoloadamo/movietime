a36aa4b14cb070b193238214b8795c74cc6646c1
aleks vangjelofski <aleks.vangjelofski@gmail.com>
2023-03-16 12:26:41 +0100

    Finalized user page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,10 @@ class UsersController < ApplicationController
   before_action :set_user, only: %i[ show ]
 
   def show
+    @requests = current_user.requests.where(status: 'accepted')
+    @events = @requests.map do |request|
+      Event.find(request.event_id)
+    end
   end
 
   private
